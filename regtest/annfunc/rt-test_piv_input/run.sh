dc6a56c852884df3532e3912ba2e1a139b79f0c3
Siva Dasetty <siva.k.dasetty@gmail.com>
2021-07-20 09:29:16 -0500

    ANN PIV reg test

---
+++ regtest/annfunc/rt-test_piv_input/run.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+#plumed driver --plumed plumed.dat --mf_pdb md.pdb >& out.out
+
+export OMP_NUM_THREADS=5
+gmx mdrun -ntomp $OMP_NUM_THREADS -s simple_run_template/3_md/md.tpr -deffnm plumed-test-md -pin on -plumed plumed.dat -v
