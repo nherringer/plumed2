9478e370d5264a243b5bd1a9d1cabb4b43c4cb7b
Siva Dasetty <siva.k.dasetty@gmail.com>
2021-07-23 12:52:35 -0500

    Updated regression test files.

--- regtest/annfunc/rt-test_piv_input/run.sh
+++ regtest/annfunc/rt-test_piv_input/run.sh
@@ -3,4 +3,8 @@
 #plumed driver --plumed plumed.dat --mf_pdb md.pdb >& out.out
 
 export OMP_NUM_THREADS=5
-gmx mdrun -ntomp $OMP_NUM_THREADS -s simple_run_template/3_md/md.tpr -deffnm plumed-test-md -pin on -plumed plumed.dat -v
+#gmx mdrun -ntomp $OMP_NUM_THREADS -s ../../../../codes/simple_run_template/3_md/md.tpr -deffnm plumed-test-md -pin on -plumed plumed.dat -v
+
+plumedFile=plumed-ann-full.dat
+gmx mdrun -ntomp $OMP_NUM_THREADS -s ../../../../codes/simple_run_template/3_md/md.tpr -deffnm plumed-test-md -pin on -plumed $plumedFile -v
+
